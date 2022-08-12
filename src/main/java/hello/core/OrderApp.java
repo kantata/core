c7bb3cc71dbef8863fc840e959d2a0cd35e4cb5c
kantata <melodics@nate.com>
2022-08-12 15:35:09 +0900

    Feat : Spring Study   - 주문시 할인 정책 적용

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrcerService;
+import hello.core.order.Order;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrcerService orcerService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP); // 멤버 생성
+        memberService.join(member); //메모리에 넣음
+
+        Order order = orcerService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order); //원래가격
+        System.out.println("order.calculatePrice() = " + order.calculatePrice()); //할인
+    }
+}
