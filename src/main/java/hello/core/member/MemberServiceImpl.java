954f1ef65c41592a937184513500be4bcb2a547d
kantata <melodics@nate.com>
2022-08-12 15:05:53 +0900

    Feat : Spring Study   - 회원 도메인

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    /**
+     * MemberServiceImpl.class
+     * MemberRepository 인터페이스를 할 (추상화 의존)
+     * MemoryMemberRepository 는 구현체를 할당 (구체화 의존)
+     * DIP 위반
+     */
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // comm
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
